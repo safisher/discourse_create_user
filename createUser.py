ec9ce5fa5d8a9267eb3be93a90de900d2247f888
Stephen Fisher <safisher>
2018-07-26 19:14:52 +0100

    removed built-in values, restructured code
    - The API and URL parameters now need to be entered in the command line.
    - Restructured the code so that the script can be imported into another python script, or run independently.

--- createUser.py
+++ createUser.py
@@ -17,20 +17,12 @@
 """
 by: S. Fisher, 2018
 
-usage: createUser.py [-h] [-c] -u username -e email [-n name]
+usage: createUser.py [-h] [-c] [-n name] -u username -e email -k api_key -a api_user -w base_url
 
-Add a new user to Discourse forum. The user will be active and approved.
+Add a new user to Discourse forum. The user will be active and approved. The program returns an exit code of 1 if the username or email exists and an exit code of 2 on error.
 
 See: https://docs.discourse.org
 """
-
-#------------------------------------------------------------------------------------------
-# REQUIRED 
-#------------------------------------------------------------------------------------------
-
-DISCOURSE_API_KEY = 'PUT YOUR API KEY HERE'
-DISCOURSE_API_USER = 'PUT YOUR API USERNAME HERE'
-DISCOURSE_URL = 'PUT YOUR URL HERE'
 
 #------------------------------------------------------------------------------------------
 # INITIALIZATIONS
@@ -47,26 +39,6 @@ VERSION = '0.1'
 PASSWORD_CHARS = string.ascii_uppercase + string.digits + string.ascii_lowercase + "(" + ")"
 PASSWORD_LENGTH = 20
 
-argParser = argparse.ArgumentParser(version=VERSION, 
-                                    description='Add user.',
-                                    formatter_class=argparse.RawDescriptionHelpFormatter,
-                                    epilog='' +
-                                    'Add a new user to Discourse forum. The user will be active and approved.\n')
-argParser.add_argument( '-c', dest='checkOnly', action='store_true', default=False,
-                        help='Check username and email without creating a new account.' )
-argParser.add_argument( '-u', dest='username', action='store', required=True, 
-                        help='Unique username.' )
-argParser.add_argument( '-e', dest='email', action='store', required=True, 
-                        help='Unique email address.' )
-argParser.add_argument( '-n', dest='fullname', action='store', required=False, default='',
-                        help='User full name. If not present then username will be used.' )
-
-clArgs = argParser.parse_args()
-if DEBUG: print clArgs
-
-# Flag if username or email exists
-checkFlag = False
-
 #------------------------------------------------------------------------------------------
 # Get URL
 #------------------------------------------------------------------------------------------
@@ -81,63 +53,70 @@ def runCommand(cmd):
     # if return code not 0 then curl failed
     if p.returncode != 0:
         sys.stderr.write('ERROR: ' + err + '\n')
-        sys.exit(0)
+        sys.exit(2)
 
     if DEBUG: print output
 
     return output
 
 #------------------------------------------------------------------------------------------
-# Check if email is unique
+# Check if email and/or username is unique
 #------------------------------------------------------------------------------------------
 
-def checkEmail():
-    global checkFlag
-
-    args = 'email=' + clArgs.email
-    args += '&api_key=' + DISCOURSE_API_KEY
-    args += '&api_username=' + DISCOURSE_API_USER
+def emailExists(email, username, apiKey, apiUser, url):
+    args = 'email=' + email
+    args += '&api_key=' + apiKey
+    args += '&api_username=' + apiUser
     
-    args = '-s -X GET "' + DISCOURSE_URL + '/admin/users/list/all.json?' + args + '" ' 
+    args = '-s -X GET "' + url + '/admin/users/list/all.json?' + args + '" ' 
     cmd = 'curl ' + args
     
     output = runCommand(cmd)    
 
     if not output == "[]" :
-        user = output.split(",")[1].split(":")[1]
-        sys.stderr.write('ERROR: Email "' + clArgs.email + '" already exists under account ' + user + ' for new user "' + clArgs.username + '".\n')
-        checkFlag = True
+        # email exists
+        existingUser = output.split(",")[1].split(":")[1].replace('"', '')
+        if existingUser != username:
+            sys.stderr.write('ERROR: Email "' + email + '" already exists under username ' + existingUser + ', not username "' + username + '".\n')
+        return True
 
-#------------------------------------------------------------------------------------------
-# Check if username is unique
-#------------------------------------------------------------------------------------------
+    return False
 
-def checkUsername():
-    global checkFlag
-
-    args = clArgs.username + '.json'
-    args += '?api_key=' + DISCOURSE_API_KEY
-    args += '&api_username=' + DISCOURSE_API_USER
-    args = '-s -X GET "' + DISCOURSE_URL + '/users/' + args + '" ' 
+def usernameExists(username, apiKey, apiUser, url):
+    args = username + '.json'
+    args += '?api_key=' + apiKey
+    args += '&api_username=' + apiUser
+    args = '-s -X GET "' + url + '/users/' + args + '" ' 
     cmd = 'curl ' + args
     
     output = runCommand(cmd)
 
     if '"error_type":"not_found"' not in output:
-        # we've already tested the username and email so this should never fail
-        sys.stderr.write('ERROR: User "' + clArgs.username + '" already exists.\n')
-        checkFlag = True
+        # username exists
+        return True
+
+    return False
+
+def isExisting(email, username, apiKey, apiUser, url):
+    # do both email and username checks before exiting program
+    usernameFlag = usernameExists(username, apiKey, apiUser, url)
+    emailFlag = emailExists(email, username, apiKey, apiUser, url)
+    if (usernameFlag and emailFlag):
+        sys.stderr.write('ERROR: User "' + username + '" and email "' + email + '" already exist.\n')
+        return True
+    elif usernameFlag:
+        sys.stderr.write('ERROR: User "' + username + '" already exist.\n')
+        return True
+    elif emailFlag:
+        # we already output the error in emailExists()
+        return True
+    return False
 
 #------------------------------------------------------------------------------------------
 # Add user
 #------------------------------------------------------------------------------------------
 
-def addUser():
-    # use username as the account name if a full name isn't provided
-    fullname = clArgs.fullname
-    if not fullname:
-        fullname = clArgs.username
-    
+def addUser(fullname, email, username, apiKey, apiUser, url):
     # generate a random password for the account
     password = ""
     i = PASSWORD_LENGTH
@@ -149,40 +128,66 @@ def addUser():
     args = 'approved=true'
     args += '&active=true'
     args += '&name=' + fullname
-    args += '&username=' + clArgs.username
-    args += '&email=' + clArgs.email
+    args += '&username=' + username
+    args += '&email=' + email
     args += '&password=' + password
-    args += '&api_key=' + DISCOURSE_API_KEY
-    args += '&api_username=' + DISCOURSE_API_USER
+    args += '&api_key=' + apiKey
+    args += '&api_username=' + apiUser
     
-    args = '-s -X POST --data "' + args + '" ' + DISCOURSE_URL + '/users'
-    cmd = 'curl ' + args
-
+    cmd = 'curl -s -X POST --data "' + args + '" ' + url + '/users'
     output = runCommand(cmd)
 
     if '"success":true' not in output:
+        # we've already tested the username and email so this should never fail
         sys.stderr.write('ERROR: creating user.\n' + output + '\n')
-        sys.exit(0)
+        sys.exit(2)
     
 # ------------------------------------------------------------------------------------------
 # Run program
 # ------------------------------------------------------------------------------------------
 
-checkUsername()
-checkEmail()
-
-# allow both email and username checks to complete before exiting program
-if checkFlag:
-    sys.exit(0)
-
-# if we got this far then we have a unique username and email address, so go ahead and add user.
-if clArgs.checkOnly:
-    print 'Unique username and email:', clArgs.username, clArgs.email
+# allow for this file to be loaded into another Python script
+if __name__ == '__main__':
+    argParser = argparse.ArgumentParser(version=VERSION, 
+                                        description='Add user.',
+                                        formatter_class=argparse.RawDescriptionHelpFormatter,
+                                        epilog='' +
+                                        'Add a new user to Discourse forum. The user will be active and approved. The program returns an exit code of 1 if the username or email exists and an exit code of 2 on error.\n')
+    argParser.add_argument( '-c', dest='checkOnly', action='store_true', default=False,
+                            help='Check username and email without creating a new account.' )
+    argParser.add_argument( '-n', dest='fullname', action='store', required=False, default='',
+                            help='User full name. If not present then username will be used.' )
+    argParser.add_argument( '-u', dest='username', action='store', required=True, 
+                            help='Unique username.' )
+    argParser.add_argument( '-e', dest='email', action='store', required=True, 
+                            help='Unique email address.' )
+    argParser.add_argument( '-k', dest='apiKey', action='store', required=True, 
+                            help='API Key.' )
+    argParser.add_argument( '-a', dest='apiUser', action='store', required=True, 
+                            help='API user.' )
+    argParser.add_argument( '-w', dest='url', action='store', required=True, 
+                            help='Discourse base URL (e.g. https://discourse.org).' )
     
-else:
-    # if we got this far then we can add the user
-    addUser()
-    print "User added:", clArgs.username
+    clArgs = argParser.parse_args()
+    if DEBUG: print clArgs
+    
+    if isExisting(clArgs.email, clArgs.username, clArgs.apiKey, clArgs.apiUser, clArgs.url):
+        sys.exit(1)
+        
+    # if we got this far then we have a unique username and email address, so go ahead and add user.
+    if clArgs.checkOnly:
+        print 'Unique username and email:', clArgs.username, clArgs.email
+    
+    else:
+        # if we got this far then we can add the user
+
+        # use username as the account name if a full name isn't provided
+        fullname = clArgs.fullname
+        if not fullname:
+            fullname = clArgs.username
+
+        addUser(fullname, clArgs.email, clArgs.username, clArgs.apiKey, clArgs.apiUser, clArgs.url)
+        print "User added:", clArgs.username
 
 
 
